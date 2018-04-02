ed8f53f2efe6835a24b5ef1b72aba9a6889e4321
Warodom <wwarodom@gmail.com>
2018-04-02 11:50:45 +0700

    router

--- node/index.js
+++ node/index.js
@@ -1,25 +1,30 @@
+// Source ==> https://github.com/wwarodom/monday
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const router = express.Router()
 
-let bears = [ {id:1, name:'Pooh', weight: 70} ,
-              {id:2, name:'Winnie', weight: 60}  ]
+let bears = [{id: 1, name: 'Pooh', weight: 70},
+    {id: 2, name: 'Winnie', weight: 60}]
 
-app.use( express.static('public'))
+app.use(express.static('public'))
 
-app.get('/bears', (req,res) => {
-    res.send(bears)
-} )
+app.use('/api', bodyParser.urlencoded({extended: false}), router)
 
-app.post('/bears',
-    bodyParser.urlencoded({extended:false}),
-    (req,res) => {
-        const bear = {}
-        bear.id = bears.length+1
-        bear.name = req.body.name
-        bear.weight = req.body.weight
-        bears.push(bear)
+router.route('/bears')
+    .get((req, res) => {
         res.send(bears)
-} )
+    })
+
+    .post(
+        (req, res) => {
+            const bear = {}
+            bear.id = bears.length + 1
+            bear.name = req.body.name
+            bear.weight = req.body.weight
+            bears.push(bear)
+            res.send(bears)
+    })
 
 app.listen(8888)
